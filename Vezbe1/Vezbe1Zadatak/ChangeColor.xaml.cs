c99af46e8c7cc166951ff5dd58d7c4971947dcd7
Kristian Tot <tot.pr89.2017@uns.ac.rs>
2021-03-13 12:31:54 +0100

    Vezbe 1 zadatak 1

---
+++ Vezbe1/Vezbe1Zadatak/ChangeColor.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vezbe1Zadatak
+{
+    /// <summary>
+    /// Interaction logic for ChangeColor.xaml
+    /// </summary>
+    public partial class ChangeColor : Window
+    {
+        public ChangeColor()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbColors.ItemsSource = typeof(Colors).GetProperties();
+        }
+
+        private void cmbColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Color selectedColor = (Color)(cmbColors.SelectedItem as PropertyInfo).GetValue(1, null);
+            MainWindow.Color = selectedColor;
+        }
+    }
+}
---
+++ Vezbe1/Vezbe1Zadatak/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vezbe1Zadatak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Color Color = Colors.Red;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lblText.Foreground = new SolidColorBrush(Color);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblText.Content = txtBox1.Text;
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void changeColor_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeColor colorWindow = new ChangeColor();
+            colorWindow.ShowDialog();
+            lblText.Foreground = new SolidColorBrush(Color);
+        }
+    }
+}
